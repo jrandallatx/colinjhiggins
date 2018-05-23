86ed16016097fd5e02e1c5d168b355deb134cfb9
Jeremy Randall <jeremyrandall1980@gmail.com>
2018-05-23 11:44:09 -0500

    Added portfolio, contact, social
    Added portfolio, contact, social, responsive image and video, started responsive divs for media queries

--- higginsfilm-productions/script.js
+++ higginsfilm-productions/script.js
@@ -24,69 +24,69 @@ var editorial = [
     ['2008', 'Barton Medical conference - editor'],
     ['2004-06', 'News Editor, CBS News KEYE-TV Austin']];
 var camera = [
-    ['2016', '"Neat Freak" (The New Movement 48-hour film competition) director of photography **winner of Funniest Film**']
-    ['2015-16', 'improv theater web/theater promos (The New Movement) camera']
-    ['2015', 'Business Coach Matthew Pollard (Golden Arm Media) camera']
-    ['2015', 'untitled house-flipping TV pilot (HGTV) camera']
-    ['2010-16', '"Cleavage Chronicles” documentary (Pamalot Productions) camera']
-    ['2010-16', '"Everybody Loves Boobs (Pamalot Productions) camera']
-    ['2014-15', '"Pomegranate Breast Massage Oil" (EarthSong PBO) camera']
-    ['2014', '"Hounds For Heroes: Dogs For PTSD Soldier" (Austin Dog Alliance) camera']
-    ['2014', '"NORC training video series" (NORC / Univ. of Chicago) camera']
-    ['2010', '“SXSW Showcase” (Vevo) camera / audio assistant']
-    ['2010', '“Platinum Weddings” (WeTV / Film Garden) camera assistant']
+    ['2016', '"Neat Freak" (The New Movement 48-hour film competition) director of photography **winner of Funniest Film**'],
+    ['2015-16', 'improv theater web/theater promos (The New Movement) camera'],
+    ['2015', 'Business Coach Matthew Pollard (Golden Arm Media) camera'],
+    ['2015', 'untitled house-flipping TV pilot (HGTV) camera'],
+    ['2010-16', '"Cleavage Chronicles” documentary (Pamalot Productions) camera'],
+    ['2010-16', '"Everybody Loves Boobs (Pamalot Productions) camera'],
+    ['2014-15', '"Pomegranate Breast Massage Oil" (EarthSong PBO) camera'],
+    ['2014', '"Hounds For Heroes: Dogs For PTSD Soldier" (Austin Dog Alliance) camera'],
+    ['2014', '"NORC training video series" (NORC / Univ. of Chicago) camera'],
+    ['2010', '“SXSW Showcase” (Vevo) camera / audio assistant'],
+    ['2010', '“Platinum Weddings” (WeTV / Film Garden) camera assistant'],
     ['2009', '“Ashley” (dir. Scott Colquitt) audition camera']];
 var directing = [
-    ['2016', 'MAC Cosmetics "Selena" product launch (ThumbWars) 'MAC artists' interview']
-    ['2016', '"Neat Freak" (The New Movement 48-hour film competition) **winner of Funniest Film**']
-    ['2015-16', 'improv theater web/theater promos (The New Movement)']
-    ['2010-16', '"Cleavage Chronicles” documentary (Pamalot Productions)']
-    ['2010-16', '"Everybody Loves Boobs stage performance (Pamalot Productions) director of media assets']
-    ['2014-15', '"Pomegranate Breast Massage Oil" (EarthSong PBO)']
-    ['2014', '"NORC training video series" (NORC / Univ. of Chicago)']
-    ['2013', '"Joanna Gonna Gett Hers" (48 Hour Film Project)']
+    ['2016', 'MAC Cosmetics "Selena" product launch (ThumbWars) \'MAC artists\' interview'],
+    ['2016', '"Neat Freak" (The New Movement 48-hour film competition) **winner of Funniest Film**'],
+    ['2015-16', 'improv theater web/theater promos (The New Movement)'],
+    ['2010-16', '"Cleavage Chronicles” documentary (Pamalot Productions)'],
+    ['2010-16', '"Everybody Loves Boobs stage performance (Pamalot Productions) director of media assets'],
+    ['2014-15', '"Pomegranate Breast Massage Oil" (EarthSong PBO)'],
+    ['2014', '"NORC training video series" (NORC / Univ. of Chicago)'],
+    ['2013', '"Joanna Gonna Gett Hers" (48 Hour Film Project)'],
     ['2005', '“You\'ve Goth Mail” (Adios MoFo/Alamo Drafthouse)']];
 var locationSound = [
-    ['2016', 'DIY group interview (Pinterest)']
-    ['2010-16', '"Cleavage Chronicles” documentary (Pamalot Productions)']
-    ['2010-16', '"Everybody Loves Boobs (Pamalot Productions)']
-    ['2014-15', '"Pomegranate Breast Massage Oil" (EarthSong PBO)']
-    ['2014', '"NORC training video series" (NORC / Univ. of Chicago)']
+    ['2016', 'DIY group interview (Pinterest)'],
+    ['2010-16', '"Cleavage Chronicles” documentary (Pamalot Productions)'],
+    ['2010-16', '"Everybody Loves Boobs (Pamalot Productions)'],
+    ['2014-15', '"Pomegranate Breast Massage Oil" (EarthSong PBO)'],
+    ['2014', '"NORC training video series" (NORC / Univ. of Chicago)'],
     ['2009', '“Ashley” (dir. Scott Colquitt)']];
 var digitalImagingTechnician = [
-    ['2013', 'SXSW ProMedia (SXSW Edu/Film/Music/Interactive) data wrangler']
-    ['2015', '"I Heart Gov" (Emergent Order) DIT']
-    ['2013', '"UT Golf Betsy Rawls Invitational" (Longhorn Network/O.K. Group) DIT']
-    ['2012', '"A Perfect Cocktail" (e-2 studios) DIT']
+    ['2013', 'SXSW ProMedia (SXSW Edu/Film/Music/Interactive) data wrangler'],
+    ['2015', '"I Heart Gov" (Emergent Order) DIT'],
+    ['2013', '"UT Golf Betsy Rawls Invitational" (Longhorn Network/O.K. Group) DIT'],
+    ['2012', '"A Perfect Cocktail" (e-2 studios) DIT'],
     ['2009', '“Chainsaw Found Jesus” (dir. Spencer Parsons) DIT']];
 var producing = [
-    ['2010-16', '"Cleavage Chronicles” documentary (Pamalot Productions) co-producer']
-    ['2011-16', '"Everybody Loves Boobs (Pamalot Productions) co-producer']
+    ['2010-16', '"Cleavage Chronicles” documentary (Pamalot Productions) co-producer'],
+    ['2011-16', '"Everybody Loves Boobs (Pamalot Productions) co-producer'],
     ['2014', '"NORC training video series" (NORC / Univ. of Chicago) producer']];
 var actorVoiceTalent = [
-    ['2015-16', 'various shows (The New Movement theater) improvisor / actor']
-    ['2011-12', '"Shipping Wars" (A&E / Megalomedia) sizzle reel actor']
-    ['2010-16', '"Everybody Loves Boobs (Pamalot Productions)']
-    ['2007', '"Friday Night Lights" series (NBCuniversal) season2 racist movie patron']
+    ['2015-16', 'various shows (The New Movement theater) improvisor / actor'],
+    ['2011-12', '"Shipping Wars" (A&E / Megalomedia) sizzle reel actor'],
+    ['2010-16', '"Everybody Loves Boobs (Pamalot Productions)'],
+    ['2007', '"Friday Night Lights" series (NBCuniversal) season2 racist movie patron'],
     ['2001', '89.7 FM KACC (Alvin Community College) Newsie / Disc Jockey']];
 var assistantDirecting = [
-    ['2007', '“On Second Thought” (dir. Curtis Pollock) 2nd assistant director']
+    ['2007', '“On Second Thought” (dir. Curtis Pollock) 2nd assistant director'],
     ['2004', '”Alternative Ending” (dir. Nick Chavez) 2nd assistant director']];
 var grip = [
-    ['2009', '“Texas Music Experience” (Crystal Media) grip']
+    ['2009', '“Texas Music Experience” (Crystal Media) grip'],
     ['2007', '“Makin\' My Move” (dir. Elizabeth Spear) grip']];
 var writing = [
-    ['2016', '"Neat Freak" (The New Movement 48-hour film competition) **winner of Funniest Film**']
+    ['2016', '"Neat Freak" (The New Movement 48-hour film competition) **winner of Funniest Film**'],
     ['2005', '“You\'ve Goth Mail” (Adios MoFo/Alamo Drafthouse) writer / director']];
 var miscellaneous = [
-    ['2015', '"Verizon Wireless Destination Next" commercial (Bishop-McCann) talent wrangler']
-    ['2012', 'Gratifon (Arts+Labor / Dell) video transciptions']
-    ['2010', '“The Real World 25” (MTV / RW Productions) casting assistant']
-    ['2009', '“Eulogy Maker” (dir. Leslie Langee) production assistant']
-    ['2009', '“F2: Forensic Factor” (Discovery Channel Canada) Texas fixer']
-    ['2006-07', 'Staff Filmmaker, DVFilm / Dominion Pictures']
-    ['2007', '“Anything For Now” (Wolfpack Group) location scout']
-    ['2006', '“Elvis and Anabelle” (Burnt Orange Productions) assistant to the director']
+    ['2015', '"Verizon Wireless Destination Next" commercial (Bishop-McCann) talent wrangler'],
+    ['2012', 'Gratifon (Arts+Labor / Dell) video transciptions'],
+    ['2010', '“The Real World 25” (MTV / RW Productions) casting assistant'],
+    ['2009', '“Eulogy Maker” (dir. Leslie Langee) production assistant'],
+    ['2009', '“F2: Forensic Factor” (Discovery Channel Canada) Texas fixer'],
+    ['2006-07', 'Staff Filmmaker, DVFilm / Dominion Pictures'],
+    ['2007', '“Anything For Now” (Wolfpack Group) location scout'],
+    ['2006', '“Elvis and Anabelle” (Burnt Orange Productions) assistant to the director'],
     ['2004', '”The Quiet” (Burnt Orange Productions) extras wrangler']];
 var pcMacSkills = [
     'Adobe Premiere',
@@ -97,6 +97,76 @@ var pcMacSkills = [
     'Avid ProTools',
     'RedGiant PluralEyes'];
 
-window.onload = function() {
+var allSections = ["xPcMacSkills", "xMiscellaneous", "xWriting", "xGrip", "xAssistantDirecting", "xActorVoiceTalent", "xProducing", "xDigitalImagingTechnician", "xLocationSound", "xDirecting", "xCamera", "xEditorial"]
+
+
+function showSection(div, data) {
     
+    for (i = 0; i < allSections.length; i++) {
+        document.getElementById(allSections[i]).innerHTML = "";
+    }
+    
+    var displayData = "<div class='display'><ul>";
+    for (i = 0; i < data.length; i++) {
+        if (Array.isArray(data[i])){
+            displayData += "<li>" + data[i][0] + ": " + data[i][1] + "</li>";
+        } else {
+            displayData += "<li>" + data[i] + "</li>";
+        }
+    }
+    displayData += "</ul></div>";
+    document.getElementById(div).innerHTML = displayData;
+}
+
+window.onload = function() {
+    console.log("loaded");
+    
+    document.getElementById("editorial").addEventListener("click", function() {
+        showSection("xEditorial", editorial);
+    });
+
+    document.getElementById("camera").addEventListener("click", function() {
+        showSection("xCamera", camera);
+    });
+
+    document.getElementById("directing").addEventListener("click", function() {
+        showSection("xDirecting", directing);
+    });
+
+    document.getElementById("locationSound").addEventListener("click", function() {
+        showSection("xLocationSound", locationSound);
+    });
+
+    document.getElementById("digitalImagingTechnician").addEventListener("click", function() {
+        showSection("xDigitalImagingTechnician", digitalImagingTechnician);
+    });
+
+    document.getElementById("producing").addEventListener("click", function() {
+        showSection("xProducing", producing);
+    });
+
+    document.getElementById("actorVoiceTalent").addEventListener("click", function() {
+        showSection("xActorVoiceTalent", actorVoiceTalent);
+    });
+
+    document.getElementById("assistantDirecting").addEventListener("click", function() {
+        showSection("xAssistantDirecting", assistantDirecting);
+    });
+
+    document.getElementById("grip").addEventListener("click", function() {
+        showSection("xGrip", grip);
+    });
+
+    document.getElementById("writing").addEventListener("click", function() {
+        showSection("xWriting", writing);
+    });
+
+    document.getElementById("miscellaneous").addEventListener("click", function() {
+        showSection("xMiscellaneous", miscellaneous);
+    });
+
+    document.getElementById("pcMacSkills").addEventListener("click", function() {
+        showSection("xPcMacSkills", pcMacSkills);
+    });
+
 }
